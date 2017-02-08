3d531e6a3668b4cac32aeeadc5495a810da83cb6
patrickthesailorman <patrickthesaiorman@gmail.com>
2017-02-08 16:54:18 -0500

    User has a role,admin and seeds

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,24 +1,24 @@
 class User < ApplicationRecord
-  rolify :before_add => :before_add_method
-
-  def before_add_method(role)
-    #do something before it gets added
-  end
-  after_create :assign_default_role
-
-  def assign_default_role
-    self.add_role(:newuser) if self.roles.blank?
-  end
+  rolify
 
 
   # Include default devise modules. Others available are:
   # :confirmable, :lockable, :timeoutable and :omniauthable
   devise :database_authenticatable, :registerable,
          :recoverable, :rememberable, :trackable, :validatable
-   has_many :posts
+   has_many :posts, dependent: :destroy
    validates :username, presence: true
    validates :password, presence: true,
               length: { minimum: 6 }
    validates :email, presence: true
    acts_as_voter
+   rolify :before_add => :before_add_method
+   after_create :assign_default_role
+
+   def before_add_method(role)
+     #do something before it gets added
+   end
+   def assign_default_role
+     self.add_role(:newuser) if self.roles.blank?
+   end
 end
