8e92cc2a01286e6c1578fef20c39185276bbddb7
patrickthesailorman <patrickthesaiorman@gmail.com>
2017-02-07 19:01:25 -0500

    add rolify

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,4 +1,14 @@
 class User < ApplicationRecord
+  rolify :before_add => :before_add_method
+  resourcify
+  def before_add_method(role)
+    #do something before it gets added
+  end
+  after_create :assign_default_role
+
+  def assign_default_role
+    self.add_role(:newuser) if self.roles.blank?
+  end
 
 
   # Include default devise modules. Others available are:
