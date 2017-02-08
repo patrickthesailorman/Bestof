7fb0823b124c5463985e14549533e9eb0616d344
patrickthesailorman <patrickthesaiorman@gmail.com>
2017-02-08 11:50:46 -0500

    role id working

--- db/seeds.rb
+++ db/seeds.rb
@@ -5,3 +5,5 @@
 #
 #   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
 #   Character.create(name: 'Luke', movie: movies.first)
+User.create(firstname: 'Robert', lastname: 'Hunter', username: 'RamblinRose', password: '123123123', email: 'rhunter@gmail.com')
+Category.create()
